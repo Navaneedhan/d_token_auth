3ce652e4f12679eed6666583ce5a5d42a4965c60
navaneedhanr <navaneedhan.ravichandran@weinvest.net>
2019-02-27 11:56:39 +0530

    test commit 5

--- app/controllers/d_token_auth/sessions_controller.rb
+++ app/controllers/d_token_auth/sessions_controller.rb
@@ -2,6 +2,9 @@ module DTokenAuth
   class SessionsController < DeviseTokenAuth::SessionsController
     def create
       puts "i am in sessions controller of new gem"
+      p resource_params
+      p resource_class
+      p resource_params.authentication_keys
       field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
       puts field
     end
