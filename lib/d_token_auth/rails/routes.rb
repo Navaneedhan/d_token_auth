b7fcb0ec105d01915d02df9e4b827b6e220814e0
navaneedhanr <navaneedhan.ravichandran@weinvest.net>
2019-02-27 18:22:12 +0530

    Fixing routes 1

--- lib/d_token_auth/rails/routes.rb
+++ lib/d_token_auth/rails/routes.rb
@@ -1,14 +1,6 @@
 module ActionDispatch::Routing
-  class Mapper (resource)
-    def mount_devise_token_auth_with_otp_verfication
-      @scope = ActionDispatch::Routing::Mapper::Scope.new(
-            path:         '',
-            shallow_path: '',
-            constraints:  {},
-            defaults:     {},
-            options:      {},
-            parent:       nil
-          )
+  class Mapper
+    def mount_devise_token_auth_with_otp_verfication(resource)
       mapping_name = resource.underscore.gsub('/', '_')
       devise_scope mapping_name.to_sym do
         post 'auth/verify_otp', controller: 'd_token_auth/sessions', action: 'verify_otp'
