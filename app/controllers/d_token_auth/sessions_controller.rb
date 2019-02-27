0966ff2c148763c4cb11987db722350b316a4dfb
navaneedhanr <navaneedhan.ravichandran@weinvest.net>
2019-02-27 11:50:57 +0530

    test commit 3

--- app/controllers/d_token_auth/sessions_controller.rb
+++ app/controllers/d_token_auth/sessions_controller.rb
@@ -2,8 +2,14 @@ module DTokenAuth
   class SessionsController < DeviseTokenAuth::ApplicationController
     def create
       puts "i am in sessions controller of new gem"
-      render_create_error_bad_credentials
+      field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
+      puts field
     end
+
+    private
+      def resource_params
+        params.permit(*params_for_resource(:sign_in))
+      end
   end
 end
   
