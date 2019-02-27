9e2b9728919387a75ea3502f09d0f7a4f6c4b575
navaneedhanr <navaneedhan.ravichandran@weinvest.net>
2019-02-27 12:16:31 +0530

    test commit 6

--- app/controllers/d_token_auth/sessions_controller.rb
+++ app/controllers/d_token_auth/sessions_controller.rb
@@ -1,12 +1,9 @@
 module DTokenAuth
   class SessionsController < DeviseTokenAuth::SessionsController
     def create
-      puts "i am in sessions controller of new gem"
-      p resource_params
-      p resource_class
-      p resource_params.authentication_keys
-      field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
-      puts field
+      super do
+        puts "i am in sessions controller of new gem"
+      end
     end
 
     # private
@@ -16,3 +13,38 @@ module DTokenAuth
   end
 end
   
+
+# def verify_otp
+#   # Check
+#   field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
+
+#   @resource = nil
+#   if field
+#     q_value = get_case_insensitive_field_from_resource_params(field)
+
+#     @resource = find_resource(field, q_value)
+#   end
+
+#   if @resource && valid_params?(field, q_value) && (!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
+#     valid_otp = @resource.valid_otp?(resource_params[:otp_value])
+#     if (@resource.respond_to?(:valid_for_authentication?) && !@resource.valid_for_authentication? { valid_otp }) || !valid_otp
+#       return render_create_error_bad_credentials
+#     end
+#     @client_id, @token = @resource.create_token
+#     @resource.save
+
+#     sign_in(:user, @resource, store: false, bypass: false)
+
+#     yield @resource if block_given?
+
+#     render_create_success
+#   elsif @resource && !(!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
+#     if @resource.respond_to?(:locked_at) && @resource.locked_at
+#       render_create_error_account_locked
+#     else
+#       render_create_error_not_confirmed
+#     end
+#   else
+#     render_create_error_bad_credentials
+#   end
+# end
