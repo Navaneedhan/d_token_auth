0c0ba46d07257d55c357dc134c7bffb8c1d7f0ef
navaneedhanr <navaneedhan.ravichandran@weinvest.net>
2019-02-28 12:44:02 +0530

    log test 6

--- app/controllers/d_token_auth/sessions_controller.rb
+++ app/controllers/d_token_auth/sessions_controller.rb
@@ -1,33 +1,40 @@
 module DTokenAuth
   class SessionsController < DeviseTokenAuth::SessionsController
     def create
-      p "i am in the begin"
-      super do |resource|
-        if DTokenAuth.otp_verfication_enabled
-          resource.decrement!(:sign_in_count)
-          sign_out :user
-          requested_at = Time.now
-          resource.trigger_otp!(requested_at)
-          return render_verify_otp_instructions
+      field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
+      @resource = nil
+      if field
+        q_value = get_case_insensitive_field_from_resource_params(field)
+
+        @resource = find_resource(field, q_value)
+      end
+
+      if @resource && valid_params?(field, q_value) && (!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
+        valid_password = @resource.valid_password?(resource_params[:password])
+        if (@resource.respond_to?(:valid_for_authentication?) && !@resource.valid_for_authentication? { valid_password }) || !valid_password
+          return render_create_error_bad_credentials
         end
-      end
-      p "current user"
-      p current_user
-      p "resource"
-      p resource
-      if resource == current_user # the resource is logged in
-        p "decrementing"
-        resource.decrement!(:sign_in_count)
-        sign_out :user
+        @client_id, @token = @resource.create_token
+        @resource.save
+
+        sign_in(:user, @resource, store: false, bypass: false) unless DTokenAuth.otp_verfication_enabled
+
+        yield @resource if block_given?
+
+        render_create_success
+      elsif @resource && !(!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
+        if @resource.respond_to?(:locked_at) && @resource.locked_at
+          render_create_error_account_locked
+        else
+          render_create_error_not_confirmed
+        end
       else
-        # do the lock strategy
+        render_create_error_bad_credentials
       end
-      p "i am in the end"
     end
 
     def verify_otp
-      return unless DTokenAuth.otp_verfication_enabled
-
+      return render_verify_otp_not_supported unless DTokenAuth.otp_verfication_enabled
       field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
       @resource = nil
       if field
