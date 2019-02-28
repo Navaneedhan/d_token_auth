ea17ebff720a0cdb88a6be9abae66e8fb40653e5
navaneedhanr <navaneedhan.ravichandran@weinvest.net>
2019-02-28 17:43:42 +0530

    test comment 1

--- lib/d_token_auth/rails/routes.rb
+++ lib/d_token_auth/rails/routes.rb
@@ -1,12 +1,12 @@
-module ActionDispatch::Routing
-  class Mapper
-    def mount_devise_token_auth_with_otp_verfication(resource)
-      mapping_name = resource.underscore.gsub('/', '_')
-      devise_scope mapping_name.to_sym do
-        post 'auth/verify_otp', controller: 'd_token_auth/sessions', action: 'verify_otp'
-        post 'auth/resend_otp', controller: 'd_token_auth/sessions', action: 'resend_otp'
-      end
-    end
+# module ActionDispatch::Routing
+#   class Mapper
+#     def mount_devise_token_auth_with_otp_verfication(resource)
+#       mapping_name = resource.underscore.gsub('/', '_')
+#       devise_scope mapping_name.to_sym do
+#         post 'auth/verify_otp', controller: 'd_token_auth/sessions', action: 'verify_otp'
+#         post 'auth/resend_otp', controller: 'd_token_auth/sessions', action: 'resend_otp'
+#       end
+#     end
 
-  end
-end
+#   end
+# end
